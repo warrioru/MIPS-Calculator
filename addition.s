dc31eeeab722f7b7ece3b3abc8e33a7cbfb8470c
Michelle Bergeron <mrb113@case.edu>
2012-03-24 20:24:08 -0400

    new .s files

---
+++ addition.s
@@ -0,0 +1,3 @@
+#This is the addition function for MIPS-Calculator
+#To be incorporated into the final calculator later
+#Actual code also to be added later
---
+++ calcFinal.s
@@ -0,0 +1,2 @@
+#This is where all of the functions will ultimately go after they're written and tested individually.
+#Authors: Michelle Bergeron, Samuel Fleckenstein, Michal Yardeni
