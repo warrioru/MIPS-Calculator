908290bd491179facbc982943f13629bb55f8ebf
Michelle Bergeron <mrb113@case.edu>
2012-03-25 11:52:15 -0400

    created simple addition program

--- addition.s
+++ addition.s
@@ -1,3 +1,25 @@
 #This is the addition function for MIPS-Calculator
 #To be incorporated into the final calculator later
-#Actual code also to be added later
+#Function computes the sum of the numbers in $t0 and $t1 and places it in $s0.
+#I don't know at all how to make it do large numbers...
+
+main:
+#Get first number from user and place in $t0
+li $v0, 5 #5 is read int syscall
+syscall #do the syscall
+move $t0, $v0  #takes the user input and puts it into $t0
+
+#Get second number from user and place in $t1
+li $v0, 5 #5 is read int syscall
+syscall #do the syscall
+move $t1, $v0  #takes the user input and puts it into $t1
+
+add $s0, $t0,  $t1 #takes sum of $t0 and $t1 and places it into $s0
+
+move $a0, $s0 #get $s0 ready to print
+li $v0, 1 #1 is the print integer syscall
+syscall #makes the syscall
+
+
+li $v0, 10 #10 is the exit syscall
+syscall #make the syscall
