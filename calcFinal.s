0bba29ea887b13775860884e19acdb7e9ff88baf
sfleckenstein <sfleckenstein159@gmail.com>
2012-04-21 19:52:11 -0300

    Update calcFinal.s

--- calcFinal.s
+++ calcFinal.s
@@ -1,9 +1,26 @@
 #This is where all of the functions will ultimately go after they're written and tested individually.
 #Authors: Michelle Bergeron, Samuel Fleckenstein, Michal Yardeni
 
- .text
+	.text
 main:
 	top:
+
+		#stores the max value (0xffffffffffffffff) in $s8
+		#this will be used to see if there will be overflow
+	addi $t0, $zero, 32767
+	addi $t1, $zero, 4
+	mult $t0, $t1
+	mflo $t2
+	addi $t2, $t2, 8
+	mult $t2, $t0
+	mflo $t3
+	addi $s8, $t3, 3
+	li $t0, 0
+	li $t1, 0
+	li $t2, 0
+	li $t3, 0
+	mult $zero, $zero
+
 			#gets the operator
 	la $a0, operatorEntry	#loads the address of operatorEntry into $a0
 	li $v0, 4		#4 is the print_string syscall
@@ -13,28 +30,28 @@ main:
 	syscall			#makes the syscall and stores the operator string
 
 	addi $t1, $zero, 43
-	beq $v0, $t2, addition
+	beq $v0, $t1, addition
 
 	addi $t1, $zero, 45
-	beq $v0, $t2, subtraction
+	beq $v0, $t1, subtraction
 
 	addi $t1, $zero, 42
-	beq $v0, $t2, multiplication
+	beq $v0, $t1, multiplication
 
 	addi $t1, $zero, 47
-	beq $v0, $t2, division
+	beq $v0, $t1, division
 
 	addi $t1, $zero, 115
-	beq $v0, $t2, computeSin
+	beq $v0, $t1, computeSin
 
 	addi $t1, $zero, 99
-	beq $v0, $t2, computeCos
+	beq $v0, $t1, computeCos
 
 	addi $t1, $zero, 116
-	beq $v0, $t2, computeTan
+	beq $v0, $t1, computeTan
 
 	addi $t1, $zero, 113
-	beq $v0, $t2, quitProgram
+	beq $v0, $t1, quitProgram
 
 end:
 	li $v0, 10		#10 is the exit syscall
@@ -60,6 +77,25 @@ addition:
 	li $v0, 5		#5 is the read_int syscall
 	syscall			#makes the syscall
 	move $t1, $v0		#moves the input into $t3
+
+	sub $s7, $s8, $t0
+	sub $s7, $s7, $t1	#s7 is now negative if there will be overflow
+
+	bltz $s7, overflowConditionAddition
+	
+returnToNormalAddition:
+	add $s0, $t0, $t1	#s0 now contains $t0 + $t1
+
+	bne $zero, $s1, printOverflowAddition
+
+printNormalAddition:
+	la $a0, equalsSign	#loads the address of userInput4 into $a0
+	li $v0, 4		#4 is the print_string syscall
+	syscall			#makes the syscall
+
+	move $a0, $s0		#have to move $s0 to $a0 because it only prints $a0
+	li $v0, 1		#1 is the print_int syscall
+	syscall			#makes the syscall
 
 	j top			#returns to the input stage
 
@@ -156,8 +192,23 @@ computeTan:
 quitProgram:
 	j end			#jumps to the end tag
 
+#################################
+	#ADDITION
+overflowConditionAddition:
+	#stores the overflow in $s1
+	#what to do here?
+	j returnToNormalAddition
+
+printOverflowAddition:
+	move $a0, $s1 	#have to move $s1 to $a0 because it only prints $a0
+	li $v0, 1	#1 is the print_int syscall
+	syscall		#makes the syscall
+	j printNormalAddition
+#################################
+
 	.data
-operatorEntry:	.asciiz "Operator:"
-userInput1:	.asciiz	"First number:"
+equalsSign:     .asciiz "= "
+operatorEntry:	.asciiz "\nOperator:"
+userInput1:	.asciiz	"\nFirst number:"
 userInput2:	.asciiz "Second number:"
-enterAngle:	.asciiz "Enter the angle in radians, using the decimal form."
+enterAngle:	.asciiz "\nEnter the angle in radians, using the decimal form."
